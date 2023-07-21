3e06c49cee8c341a3b351560733b33184d99425f
Andrew Liu <andelerliutv@gmail.com>
2023-07-21 11:14:25 -0700

    added loader.io

--- Database/Postgres/routes.js
+++ Database/Postgres/routes.js
@@ -7,6 +7,9 @@ const controllers = require('./Controllers/controllers.js')
 router.get(`/products`, controllers.getAllProducts)
 router.get(`/product/:product_id`, controllers.getProductById)
 router.get('/product/:product_id/styles', controllers.getStylesById)
+app.get("/loaderio-d07120ba7a9e99cf54edf563521476bc", (req, res) => 
+    res.send("loaderio-d07120ba7a9e99cf54edf563521476bc")
+)
 router.post('/product/:product_id/addCart', controllers.addCart)
 router.post('/product/:product_id/getCard', controllers.getCart)
 
